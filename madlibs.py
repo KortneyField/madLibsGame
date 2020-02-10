34b6b45493df7f447f6260893639bb6904a47a22
Kortney <kfiel48@wgu.edu>
2020-02-10 13:51:00 -0600

    added code to file

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your friend's NAME: ")
+noun = input("Enter a plural NOUN:  ")
+verb = input("Enter a VERB:  ")
+num = input("Enter a NUMBER:   ")
+
+print('''
+Yesterday {} went to the park.
+They love to play with {} when they visit while they {}.
+They always pack {} apples to eat.
+'''.format(name,noun,verb,num))
